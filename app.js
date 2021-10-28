fc7ffda34e6d924d9577fac8ffa50391caa43798
Andretreccia <andreabtreccia@gmail.com>
2021-10-28 15:40:54 +0200

    Inserita immagine con vujs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: '#root',
     data: {
-        title: "Messaggio esercizio VUE-HELLO!"
+        title: "Messaggio esercizio VUE-HELLO!",
+        image: "https://www.cinquecosebelle.it/wp-content/uploads/2015/11/darth.jpg",
     },
 })
